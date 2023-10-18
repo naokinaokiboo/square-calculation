00c4b85640d4c69df9f9fdab2af98f17e1a922af
naokinaokiboo <naokinaokiboo@gmail.com>
2023-10-18 14:19:20 +0900

    ターミナルの幅、高さが足りない場合のメッセージを追加

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 import SquareCalculation from "./square-calculation.js";
 
-new SquareCalculation().execute();
+const REQUIRED_TERMINAL_HEIGHT = 26;
+const REQUIRED_TERMINAL_WIDTH = 55;
+
+const terminalHeight = process.stdout.rows;
+const terminalWidth = process.stdout.columns;
+
+if (
+  terminalHeight < REQUIRED_TERMINAL_HEIGHT ||
+  terminalWidth < REQUIRED_TERMINAL_WIDTH
+) {
+  console.log("The terminal's width and height are insufficient. ");
+  console.log("Please enlarge the terminal and run again.");
+  console.log("The required width and height are 55 and 26, respectively.");
+  console.log(
+    `(Current width:${terminalWidth}, Current height:${terminalHeight})`
+  );
+} else {
+  new SquareCalculation().execute();
+}
