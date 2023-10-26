9df04a7fdacb1b8a22c063768e9da0870414a953
naokinaokiboo <naokinaokiboo@gmail.com>
2023-10-26 19:58:09 +0900

    メニューの表示機能をindex.jsに分離

--- index.js
+++ index.js
@@ -1,23 +1,109 @@
 #!/usr/bin/env node
 
+import enquirer from "enquirer";
 import SquareCalculation from "./square-calculation.js";
+import Formatter from "./formatter.js";
+
+export const MENU_ID_EASY_MODE = 1;
+export const MENU_ID_NORMAL_MODE = 2;
+const MENU_ID_QUIT = 3;
 
 const REQUIRED_TERMINAL_HEIGHT = 26;
 const REQUIRED_TERMINAL_WIDTH = 55;
 
-const terminalHeight = process.stdout.rows;
-const terminalWidth = process.stdout.columns;
+const hasSufficientTerminalSize = (terminalHeight, terminalWidth) => {
+  return (
+    terminalHeight < REQUIRED_TERMINAL_HEIGHT ||
+    terminalWidth < REQUIRED_TERMINAL_WIDTH
+  );
+};
 
-if (
-  terminalHeight < REQUIRED_TERMINAL_HEIGHT ||
-  terminalWidth < REQUIRED_TERMINAL_WIDTH
-) {
-  console.log("The terminal's width and height are insufficient. ");
-  console.log("Please enlarge the terminal and run again.");
-  console.log("The required width and height are 55 and 26, respectively.");
-  console.log(
+const warningMessage = (terminalHeight, terminalWidth) => {
+  return (
+    "The terminal's width and height are insufficient. \n" +
+    "Please enlarge the terminal and run again.\n" +
+    `The required width and height are ${REQUIRED_TERMINAL_WIDTH} and ${REQUIRED_TERMINAL_HEIGHT}, respectively.\n` +
     `(Current width:${terminalWidth}, Current height:${terminalHeight})`
   );
-} else {
-  new SquareCalculation().execute();
+};
+
+const showMenus = async () => {
+  const menus = [
+    {
+      id: MENU_ID_EASY_MODE,
+      name: "Easy Mode (5 x 5 Grid)",
+      description: "Start the game with 25 squares.",
+    },
+    {
+      id: MENU_ID_NORMAL_MODE,
+      name: "Normal Mode (10 x 10 Grid)",
+      description: "Start the game with 100 squares.",
+    },
+    {
+      id: MENU_ID_QUIT,
+      name: "Quit",
+      description: "Exit menu.",
+    },
+  ];
+  const question = {
+    type: "select",
+    name: "value",
+    message: "Please select a menu.",
+    choices: menus.map((menu) => ({
+      name: menu.name,
+      value: menu.id,
+    })),
+    result() {
+      return this.focused.value;
+    },
+    footer() {
+      const descriptions = menus.map((mode) => mode.description);
+      return "\n  " + descriptions[this.index];
+    },
+  };
+
+  console.clear();
+  console.log(Formatter.generateHeader());
+  try {
+    const answer = await enquirer.prompt(question);
+    return answer.value;
+  } catch (err) {
+    console.error("An unexpected error has occurred.");
+    process.exit(1);
+  }
+};
+
+export const main = async () => {
+  const terminalHeight = process.stdout.rows;
+  const terminalWidth = process.stdout.columns;
+  if (hasSufficientTerminalSize(terminalHeight, terminalWidth)) {
+    console.log(warningMessage(terminalHeight, terminalWidth));
+    return;
+  }
+
+  const selectedMenuId = await showMenus();
+  try {
+    switch (selectedMenuId) {
+      case MENU_ID_EASY_MODE:
+      case MENU_ID_NORMAL_MODE:
+        new SquareCalculation(selectedMenuId);
+        break;
+      case MENU_ID_QUIT:
+        process.exit(0);
+        break;
+      default:
+        throw new Error("Unknown menu is selected.");
+        break;
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+    } else {
+      throw err;
+    }
+  }
+};
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+  main();
 }
--- square-calculation.js
+++ square-calculation.js
@@ -1,11 +1,7 @@
-import enquirer from "enquirer";
+import { MENU_ID_EASY_MODE, MENU_ID_NORMAL_MODE, main } from "./index.js";
 import Position from "./position.js";
 import Formatter from "./formatter.js";
 import InputCursor from "./input-cursor.js";
-
-const MENU_ID_EASY_MODE = 1;
-const MENU_ID_NORMAL_MODE = 2;
-const MENU_ID_QUIT = 3;
 
 const DIMENTION_OF_EASY_MODE = 5;
 const DIMENTION_OF_NORMAL_MODE = 10;
@@ -18,76 +14,13 @@ class SquareCalculation {
   #matrix;
   #mistakes;
 
-  async execute() {
-    const menuId = await this.#showMenus();
-    try {
-      switch (menuId) {
-        case MENU_ID_EASY_MODE:
-          this.#startNewGame(DIMENTION_OF_EASY_MODE);
-          break;
-        case MENU_ID_NORMAL_MODE:
-          this.#startNewGame(DIMENTION_OF_NORMAL_MODE);
-          break;
-        case MENU_ID_QUIT:
-          process.exit(0);
-          break;
-        default:
-          throw new Error("Unknown menu is selected.");
-          break;
-      }
-    } catch (err) {
-      console.error(err.message);
-      process.exit(1);
+  constructor(menuId) {
+    let dimention;
+    if (menuId === MENU_ID_EASY_MODE) {
+      dimention = DIMENTION_OF_EASY_MODE;
+    } else if (menuId === MENU_ID_NORMAL_MODE) {
+      dimention = DIMENTION_OF_NORMAL_MODE;
     }
-  }
-
-  async #showMenus() {
-    const menus = [
-      {
-        id: MENU_ID_EASY_MODE,
-        name: "Easy Mode (5 x 5 Grid)",
-        description: "Start the game with 25 squares.",
-      },
-      {
-        id: MENU_ID_NORMAL_MODE,
-        name: "Normal Mode (10 x 10 Grid)",
-        description: "Start the game with 100 squares.",
-      },
-      {
-        id: MENU_ID_QUIT,
-        name: "Quit",
-        description: "Exit menu.",
-      },
-    ];
-    const question = {
-      type: "select",
-      name: "value",
-      message: "Please select a menu.",
-      choices: menus.map((menu) => ({
-        name: menu.name,
-        value: menu.id,
-      })),
-      result() {
-        return this.focused.value;
-      },
-      footer() {
-        const descriptions = menus.map((mode) => mode.description);
-        return "\n  " + descriptions[this.index];
-      },
-    };
-
-    this.#updateDisplay();
-    let answer;
-    try {
-      answer = await enquirer.prompt(question);
-    } catch (err) {
-      console.error("An unexpected error has occurred.");
-      process.exit(1);
-    }
-    return answer.value;
-  }
-
-  #startNewGame(dimention) {
     this.#dimention = dimention;
     this.#position = new Position(dimention);
     this.#formatter = new Formatter(dimention);
@@ -95,6 +28,24 @@ class SquareCalculation {
     this.#initMatrix();
     this.#updateDisplay();
     this.#inputCursor.startKeyInput(this.update.bind(this));
+  }
+
+  update(numStr) {
+    const num = Number.parseInt(numStr);
+    if (isNaN(num)) {
+      return;
+    }
+
+    this.#matrix[this.#position.row()][this.#position.column()] = num;
+    this.#position.update();
+    this.#inputCursor.updateCursorPosition();
+
+    if (this.#hasGameEnded()) {
+      this.#inputCursor.stopKeyInput();
+      this.#checkAnswer();
+      this.#updateDisplay();
+      this.#waitForSomeKeyInput();
+    }
   }
 
   #initMatrix() {
@@ -127,24 +78,6 @@ class SquareCalculation {
     }
   }
 
-  update(numStr) {
-    const num = Number.parseInt(numStr);
-    if (isNaN(num)) {
-      return;
-    }
-
-    this.#matrix[this.#position.row()][this.#position.column()] = num;
-    this.#position.update();
-    this.#inputCursor.updateCursorPosition();
-
-    if (this.#hasGameEnded()) {
-      this.#inputCursor.stopKeyInput();
-      this.#checkAnswer();
-      this.#updateDisplay();
-      this.#waitForSomeKeyInput();
-    }
-  }
-
   #hasGameEnded() {
     return this.#position.row() > this.#dimention;
   }
@@ -167,18 +100,8 @@ class SquareCalculation {
     process.stdin.once("data", (_key) => {
       process.stdin.pause();
       process.stdin.removeAllListeners("data");
-      this.#releaseAllResources();
-      this.execute();
+      main();
     });
-  }
-
-  #releaseAllResources() {
-    this.#dimention = null;
-    this.#position = null;
-    this.#inputCursor = null;
-    this.#formatter = null;
-    this.#matrix = null;
-    this.#mistakes = null;
   }
 }
 
